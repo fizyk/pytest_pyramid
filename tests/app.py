20ebf9b85ecdbe8b89d8fd31b9bca1ce55fd9f2f
Grzegorz Śliwiński <fizyk@fizyk.net.pl>
2014-01-16 20:49:57 +0100

    setup.cfg for wheels

--- tests/app.py
+++ tests/app.py
@@ -3,5 +3,5 @@ from pyramid.config import Configurator
 
 
 def main(global_config, **settings):
-	config = Configuratorsettings)
-	return config
+    config = Configurator(settings)
+    return config
