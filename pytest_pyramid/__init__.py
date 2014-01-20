63deafa2a6278aeb2d3d3208506d3561e02878bd
Grzegorz Śliwiński <fizyk@fizyk.net.pl>
2014-01-20 20:57:38 +0100

    "Release 0.1.1"

--- pytest_pyramid/__init__.py
+++ pytest_pyramid/__init__.py
@@ -5,4 +5,4 @@
 # This module is part of pytest_pyramid and is released under
 # the MIT License (MIT): http://opensource.org/licenses/MIT
 
-__version__ = '0.1.0'  # pragma: no cover
+__version__ = '0.1.1'  # pragma: no cover
